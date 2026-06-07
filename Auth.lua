b45408989c6116c65b81aface612327d8bfebc80
Quadra <bryanhadson3@gmail.com>
2026-06-07 14:10:16 +0700

    Create Auth.lua

---
+++ Auth.lua
@@ -0,0 +1,12 @@
+--[[ 
+   _____                                 ____                             
+  / ___/__  ______  ____ _____  _______ / __ \\____ _____  ________  _____
+  \\__ \\/ / / / __ \\/ __ \\`/ __ \\/ ___/ // /_/ / __ \\`/ __ \\/ ___/ _ \\/ ___/
+ ___/ / /_/ / / / / /_/ / /_/ (__  ) // _, _/ /_/ / / / / /__/  __/ /    
+/____/\\__, /_/ /_/\\__,_/ .___/____(_)/ /_/ |_|\\__,_/_/ /_/\\___/\\___/_/     
+     /____/           /_/          /___/                                   
+        This script obfuscated with @SynapseDancer (SYNDX) obf. — Always use loadstring! <3
+]]
+local _w="@luauth:script_5048319459338906249:build_mq3fxs7c_21aa8361"
+local _c="0416706809"
+loadstring(game:HttpGet("https://luauth.org/files/v2/loaders/041e6db70b6c8e094bd0d5018057dab3.lua"), _w)()
